c4b61decf4e1b51e627b80973f13b0074c1696bd
nathan jobs <nathanmbuma51@gmail.com>
2022-03-04 19:24:32 +0100

    7 integrations tests

--- Model/app.py
+++ Model/app.py
@@ -1,4 +1,5 @@
 from detoxify import Detoxify
+
 import json
 from flask import Flask, request
 from dotenv import load_dotenv
@@ -7,22 +8,17 @@ load_dotenv()
 
 model = Detoxify('original')
 app = Flask(__name__)
-@app.route('/',methods=['POST']) 
-def predict():
-    value =  request.json['sentences']
-    results = model.predict(value)
+@app.route('/',methods=['POST'])
+def get_data():
+    return prediction(request.json['sentences'])
+
+def prediction(sentence):
+   
+    results = model.predict(sentence)
     converterResults = {
                         label: f'{percentage:.4f}'
                         for label, percentage in results.items()
                         }
     return json.dumps(converterResults)
- 
-'''
-def main():
-    filename = "loaded_model.pkl"
-    #pickle.dump(model,open(filename,'wb'))
-    predict('I hate you because you are lazy')
 
-if __name__ == '__main__':
-    main()
-'''
+ 
---
+++ Tests/integration_test.py
@@ -0,0 +1,45 @@
+
+import sys
+from urllib import response 
+
+sys.path.append('..')
+
+from Backend.app import *
+
+from Model.app import *
+
+from Frontend.app import *
+sys.path.append('Frontend/static/css')
+
+
+class TestApp:
+    model = Detoxify('original')
+    sentencePositive = "I love human"
+    sentenceNegative = 'I hate you because you are lazy'
+    sentenceNeutral = 'you are my dog'
+    toxicityScore = 59
+    dictionaryTotransform = {'toxicity': '0.9653', 'severe_toxicity': '0.0052', 'obscene': '0.0797', 'threat': '0.0027', 'insult': '0.6963', 'identity_attack': '0.0158'}
+ 
+
+    def test_port_front(self):
+        response =  requests.get('http://localhost:5001')
+        assert response.status_code == 200
+    
+    
+    def test_port_back(self):
+        response =  requests.get('http://localhost:5000')
+        assert response.status_code == 405
+
+    def test_port_model(self):
+        response =  requests.get('http://localhost:5002')
+        assert response.status_code == 405
+    
+    
+    def test_back_to_front(self):
+        response = requests.post('http://localhost:5001', data=json.dumps({'sentences': self.dictionaryTotransform}))
+        assert response.status_code == 405
+
+    def test_boostrap_css(self):
+        response = requests.get('http://static/css/bootstrap/bootstrap.min.css')
+        assert response.status_code == 304
+        
