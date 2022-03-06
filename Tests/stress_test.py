0a41f03d33b644b6d570c15c5421a3b55fc452dd
nathan jobs <nathanmbuma51@gmail.com>
2022-03-06 09:01:01 +0100

    1000 requests in less than a minute

---
+++ Tests/stress_test.py
@@ -0,0 +1,22 @@
+import sys
+import time 
+
+sys.path.append('..')
+
+from Backend.app import *
+
+from Model.app import *
+
+from Frontend.app import *
+
+class TestApp:
+    sentencePositive = "I love human"
+   
+    def test_stress(self):
+        start = time.time()
+        for i in range(1000):
+            #front ask data to back and back ask prediction to model. 
+            request = requests.post('http://localhost:5000', data=json.dumps({'sentences': self.sentencePositive}), headers={'content-type': 'application/json'} )
+            json.loads(request.content.decode('utf-8')) #prediction
+        end = time.time()
+        assert((end-start)/1000) < 0.1
